7488463dd91fe5ea184b44d049d1e22c6c5856e3
Jonathan1324 <mohrjonathan09@gmail.com>
2025-06-19 16:19:21 +0200

    better testing

--- tests/test.asm
+++ tests/test.asm
@@ -3,8 +3,11 @@
 section .text
 global _start
 _start:
-    mov eax, [data1]
-
-section .data
-data1:          dd 0x01234567
-data2:          dd 0x89abcdef
+    mov eax, 0x12345678
+    mov ebx, 1
+    mov ecx, 2
+    mov edx, 11
+    mov esp, 143
+    mov ebp, 25
+    mov esi, 123456
+    mov edi, 50
