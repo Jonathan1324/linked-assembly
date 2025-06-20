1048db7ea64496abf36324f275536635166aa6ae
Jonathan1324 <mohrjonathan09@gmail.com>
2025-06-20 20:58:33 +0200

    added test program

--- tests/test.asm
+++ tests/test.asm
@@ -3,12 +3,8 @@
 section .text
 global _start
 _start:
-    int 0b1010
-    int 0d10
-    int 0h10
-    int 0o10
-    int 0q10
-    int 0t10
-    int 0x10
-    int 0y10
-    int 10
+
+    mov eax, 1
+    mov ebx, 0
+
+    int 0x80
