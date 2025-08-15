74709843e7c919fd3c01a16e916f9aa11dbcc8c7
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-15 17:04:38 +0200

    .

--- tests/srcs/test.asm
+++ tests/srcs/test.asm
@@ -6,18 +6,7 @@ section .text
     global _start
 
 _start:
-    ;mov eax, 1
-    ;mov ebx, 0
-    int 0
-
-    ;lea eax, err
-
-section .data
-
-;msg_start equ $
-msg db "Hello, World!", ENDL
-;msg_len equ $ - msg
-
-err db "Error found!", ENDL
-
-db 0xff, 0xff, $, $
+    
+    
+section .data    
+test:   dd 0x12345678
