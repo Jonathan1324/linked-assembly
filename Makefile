5b1476d468e26081ecd12a6e4aba200f4a5df0f2
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-15 12:32:21 +0200

    added ci

--- Makefile
+++ Makefile
@@ -35,19 +35,17 @@ libcore:
 		SRC_DIR=$(SRC_DIR)/lib 				\
 		LIB_DIR=$(LIB_DIR)					\
 		LIB=core							\
-		BUILD_DIR=$(BUILD_DIR)/lib			\
-		BIN_DIR=$(BIN_DIR)
+		BUILD_DIR=$(BUILD_DIR)/lib
 
 librust:
-	$(MAKE) -C $(SRC_DIR)/rust				\
+	@$(MAKE) -C $(SRC_DIR)/rust				\
 		DEBUG=$(DEBUG)						\
 											\
 		RUSTFLAGS="$(RUSTFLAGS)"			\
 		SRC_DIR=$(SRC_DIR)/rust 			\
 		LIB_DIR=$(LIB_DIR)					\
 		LIB=rust							\
-		BUILD_DIR=$(BUILD_DIR)/rust			\
-		BIN_DIR=$(BIN_DIR)
+		BUILD_DIR=$(BUILD_DIR)/rust
 
 asmp: libcore librust
 	@$(MAKE) -C $(SRC_DIR)/asmp 			\
@@ -59,7 +57,6 @@ asmp: libcore librust
 		LDFLAGS="$(LDFLAGS) $(LDFLAGSSRC)"	\
 		SRC_DIR=$(SRC_DIR)/asmp		 		\
 		BUILD_DIR=$(BUILD_DIR)/asmp			\
-		BIN_DIR=$(BIN_DIR)					\
 		LIB_DIR=$(LIB_DIR)					\
 		EXE_EXT=$(EXE_EXT)
 
@@ -73,7 +70,6 @@ assembler: libcore librust
 		LDFLAGS="$(LDFLAGS) $(LDFLAGSSRC)"	\
 		SRC_DIR=$(SRC_DIR)/assembler 		\
 		BUILD_DIR=$(BUILD_DIR)/assembler	\
-		BIN_DIR=$(BIN_DIR)					\
 		LIB_DIR=$(LIB_DIR)					\
 		EXE_EXT=$(EXE_EXT)
 
@@ -87,7 +83,6 @@ linker: libcore librust
 		LDFLAGS="$(LDFLAGS) $(LDFLAGSSRC)"	\
 		SRC_DIR=$(SRC_DIR)/linker 			\
 		BUILD_DIR=$(BUILD_DIR)/linker		\
-		BIN_DIR=$(BIN_DIR)					\
 		LIB_DIR=$(LIB_DIR)					\
 		EXE_EXT=$(EXE_EXT)
 
@@ -117,4 +112,3 @@ clean:
 		BUILD_DIR=$(BUILD_DIR)/rust
 
 	@rm -rf $(LIB_DIR)
-	@rm -rf bin
--- ci/ci.py
+++ ci/ci.py
@@ -7,42 +7,61 @@ import sys
 
 # Logger
 Path("logs").mkdir(exist_ok=True)
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers= [
-        logging.FileHandler("logs/ci.log", mode="w", encoding="utf-8"),
-        logging.StreamHandler()
-    ]
-)
+
+logger = logging.getLogger("ci")
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("logs/ci.log", mode="w", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = logging.Formatter("[%(levelname)s] %(message)s")
+console_handler.setFormatter(console_formatter)
+logger.addHandler(console_handler)
 
 # Argument parser
 parser = argparse.ArgumentParser(description="CI/CD Script")
 parser.add_argument(
     "-d", "--debug",
+    dest="debug",
     action="store_true",
     help="Enable debug build"
 )
 parser.add_argument(
     "-c", "--clean",
+    dest="clean",
     action="store_true",
     help="Clean builds"
 )
 parser.add_argument(
     "--no-build",
-    action="store_true",
+    dest="build",
+    action="store_false",
     help="Stop before building the project"
 )
 
 if __name__ == "__main__":
+    help = False
     if any(arg.startswith("-h") or arg in ("--h", "--he", "--hel", "--help") for arg in sys.argv[1:]):
-        logging.info("Showing help message")
+        help = True
 
+    logger.debug(f"Help: {help}")
+
+    if (not help): logger.debug("Parsing through the args")
     args = parser.parse_args()
-    logging.info(f"Debug: {args.debug}, Clean: {args.clean}, Build: {not args.no_build}")
+    logger.debug(f"Debug: {args.debug}, Clean: {args.clean}, Build: {args.build}")
 
-    if (args.clean): clean(debug=args.debug)
+    if (args.clean):
+        logger.debug("Cleaning")
+        clean(debug=args.debug)
 
-    if (args.no_build): sys.exit(0)
+    if (not args.build):
+        logger.debug("Stopping before building")
+        sys.exit(0)
 
+    logger.info("Building the project")
     build(debug=args.debug)
