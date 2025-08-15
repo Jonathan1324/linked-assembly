448bddfaa8c13c66f11cc361f0c43cb48ef4b825
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-15 22:01:18 +0200

    .

--- ci/build.py
+++ ci/build.py
@@ -7,12 +7,12 @@ def build(debug: bool) -> bool:
     logger.info("Building the project")
     cmd = ["make"]
     if (debug): cmd.append("DEBUG=1")
-    subprocess.run(cmd)
-    return True
+    result = subprocess.run(cmd)
+    return result.returncode == 0
 
 def clean(debug: bool) -> bool:
     logger.info("Cleaning")
     cmd = ["make", "clean"]
     if (debug): cmd.append("DEBUG=1")
-    subprocess.run(cmd)
-    return True
+    result = subprocess.run(cmd)
+    return result.returncode == 0
