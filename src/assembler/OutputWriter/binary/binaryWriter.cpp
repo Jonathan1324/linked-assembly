5991b6a129e399bf627785cd1ea7f920263cf022
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-18 10:41:52 +0200

    .

--- src/assembler/OutputWriter/binary/binaryWriter.cpp
+++ src/assembler/OutputWriter/binary/binaryWriter.cpp
@@ -1,7 +1,7 @@
 #include "BinaryWriter.hpp"
 #include <limits>
 #include <memory>
-#include <string>
+#include <cstring>
 
 Binary::Writer::Writer(const Context &_context, Architecture _arch, BitMode _bits, Format _format, std::ostream *_file, const Parser::Parser *_parser, const Encoder::Encoder *_encoder)
     : ::Output::Writer::Writer(_context, _arch, _bits, _format, _file, _parser, _encoder)
