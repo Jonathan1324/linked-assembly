9c2d57bac9d7ee5854140d2f89184f36b4a2b348
Jonathan1324 <mohrjonathan09@gmail.com>
2025-08-18 12:01:11 +0200

    .

--- scripts/os.mk
+++ scripts/os.mk
@@ -20,9 +20,9 @@ else
 endif
 
 ifeq ($(OS),Windows_NT)
+    LDFLAGS += -lws2_32 -luser32 -lkernel32 -lwsock32 -lntdll -luserenv
     ifeq ($(UNAME_M),amd64)
         RUST_TARGET = x86_64-pc-windows-gnu
-        LDFLAGS += -lws2_32 -luser32 -lkernel32 -lwsock32 -lntdll
     else ifeq ($(UNAME_M),arm64)
         RUST_TARGET = aarch64-pc-windows-gnu
     endif
