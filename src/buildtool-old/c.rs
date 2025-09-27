809303b9d984a1182b0d92156bf688558cf3ba76
Jonathan1324 <mohrjonathan09@gmail.com>
2025-09-27 15:02:42 +0200

    deleted old buildtool

--- src/buildtool-old/c.rs
+++
@@ -1,8 +0,0 @@
-unsafe extern "C" {
-    pub fn printVersion();
-    pub fn ParseCacheFile(path: *const libc::c_char) -> u64;
-    pub fn WriteCacheFile(buf_ptr: u64, path: *const libc::c_char);
-    pub fn FreeCacheBuffer(buf_ptr: u64);
-    pub fn AddToCache(but_ptr: u64, name: *const libc::c_char, name_length: u64, value: *const libc::c_char, value_length: u64);
-    pub fn ReadFromCache(buf_ptr: u64, name: *const libc::c_char, name_length: u64, value_length: *mut u64) -> *const libc::c_char;
-}
