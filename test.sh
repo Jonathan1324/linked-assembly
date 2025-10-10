34b32b5475f7861b533ff784e9a6a54d62d5e098
Jonathan1324 <mohrjonathan09@gmail.com>
2025-10-10 17:12:44 +0200

    finished

--- test.sh
+++ test.sh
@@ -19,22 +19,3 @@ find "tests/assembler/build/nasm/nasm" -type f -name "*.o" | while read -r file;
     mkdir -p "$(dirname "$log_file")"
     objdump --all-headers "$file" > "$log_file" 2>&1
 done
-
-#
-## OS erkennen
-#OS=$(uname)
-#
-#if [ "$OS" = "Linux" ]; then
-#    ld -m elf_i386 -o tests/assembler/build/elf/test-32bit tests/assembler/build/test.asm-x86-32bit-elf.o >> logs/assembler/elf/ld-test-32bit.txt 2>&1
-#    ld -m elf_x86_64 -o tests/assembler/build/elf/test-64bit tests/assembler/build/test.asm-x86-64bit-elf.o >> logs/assembler/elf/ld-test-64bit.txt 2>&1
-#
-#    chmod +x tests/assembler/build/elf/test-32bit
-#    chmod +x tests/assembler/build/elf/test-64bit
-#else
-#    echo "[WARNING] Can't link on this OS: $OS" >> logs/assembler/elf/ld.txt 2>&1
-#    echo "[WARNING] Can't link on this OS: $OS"
-#    exit 0
-#fi
-
-#tests/assembler/build/elf/test-32bit
-#echo "exit code: $?"
