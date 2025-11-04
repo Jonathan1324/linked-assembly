cc7266ec3acddf14ff86b1bedf015f1ca55e78cf
Jonathan1324 <mohrjonathan09@gmail.com>
2025-11-04 15:49:59 +0100

    updated tests

--- test_lfs.sh
+++ test_lfs.sh
@@ -4,20 +4,12 @@ python3 -m ci.ci
 
 cd tests/lfs
 
-mkdir -p build/mkfs
-dd if=/dev/zero of=build/mkfs/fat12.img bs=1024 count=1440
-mkfs.fat -F 12 build/mkfs/fat12.img -s 1 -S 512 -i 0x12345678
-dd if=/dev/zero of=build/mkfs/fat16.img bs=1M count=16
-mkfs.fat -F 16 build/mkfs/fat16.img -i 0x12345678
-dd if=/dev/zero of=build/mkfs/fat32.img bs=1M count=100
-mkfs.fat -F 32 build/mkfs/fat32.img -i 0x12345678
-
-mkdir -p build/test
-
-../../dist/bin/lfs create build/test/fat12.img --type fat12 --root roots/test --size 1440K
-
-../../dist/bin/lfs create build/test/fat16.img --type fat16 --root roots/test --size 16M
-
-../../dist/bin/lfs create build/test/fat32.img --type fat32 --root roots/test --size 100M
+mkdir -p build/test_mkfs
+dd if=/dev/zero of=build/test_mkfs/fat12.img bs=1024 count=1440
+mkfs.fat -F 12 build/test_mkfs/fat12.img -s 1 -S 512 -i 0x12345678
+dd if=/dev/zero of=build/test_mkfs/fat16.img bs=1M count=16
+mkfs.fat -F 16 build/test_mkfs/fat16.img -i 0x12345678
+dd if=/dev/zero of=build/test_mkfs/fat32.img bs=1M count=100
+mkfs.fat -F 32 build/test_mkfs/fat32.img -i 0x12345678
 
 cd ../..
--- tests/lfs/test.py
+++ tests/lfs/test.py
@@ -7,7 +7,24 @@ logger = logging.getLogger("tests")
 
 def test(dir: Path, log_dir: Path):
     lfs = Path("dist/bin/lfs")
-    src_dir = dir / "srcs"
+    root_dir = dir / "roots"
+
+    roots = [p for p in root_dir.iterdir() if p.is_dir() and p.name != ".ignore"]
+    for root in roots:
+        for fs_type in ["fat12", "fat16", "fat32"]:
+            log_path = Path(f"{log_dir}/{root.name}-{fs_type}.txt")
+            log_path.parent.mkdir(parents=True, exist_ok=True)
+
+            out_path = Path(f"{dir}/build/{root.name}/{fs_type}.img")
+            out_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(log_path, "w") as f:
+                size = "0"
+                if   fs_type == "fat12": size = "1440K"
+                elif fs_type == "fat16": size = "16M"
+                elif fs_type == "fat32": size = "100M"
+                cmd = [str(lfs), "create", str(out_path), "--type", fs_type, "--root", str(root), "--size", size]
+                subprocess.run(cmd, stdout=f, stderr=f)
 
 def clean(dir: Path, log_dir: Path):
     if log_dir.exists() and log_dir.is_dir():
