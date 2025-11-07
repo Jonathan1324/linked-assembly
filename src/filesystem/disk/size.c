844760f944fe0493aec5402f47b91fdfc942faeb
Jonathan1324 <mohrjonathan09@gmail.com>
2025-11-07 21:45:40 +0100

    f2

--- src/filesystem/disk/size.c
+++ src/filesystem/disk/size.c
@@ -7,19 +7,17 @@ uint64_t Path_GetSize(const char* path)
 
     if (FileSeek(f, 0, SEEK_END) != 0) {
         fclose(f);
-        return NULL;
+        return 0;
     }
 
     file_offset_t size = FileTell(f);
     if (FileSeek(f, 0, SEEK_SET) != 0) {
         fclose(f);
-        return NULL;
+        return 0;
     }
 
-    if (size < 0) {
-        fclose(f);
-        return NULL;
-    }
+    fclose(f);
 
+    if (size < 0) return 0;
     return (uint64_t)size;
 }
