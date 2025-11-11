95a5de07514376fbb9a7860bb9e35ed52b174ad5
Jonathan1324 <mohrjonathan09@gmail.com>
2025-11-11 14:19:18 +0100

    .

--- test_lfs.sh
+++ test_lfs.sh
@@ -10,21 +10,21 @@ OS=$(uname)
 
 dd if=/dev/zero of=build/test_mkfs/fat12.img bs=1024 count=1440
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 12 -i 0x12345678 build/test_mkfs/fat12.img
+    newfs_msdos -F 12 -I 0x12345678 build/test_mkfs/fat12.img
 else
     mkfs.fat -F 12 build/test_mkfs/fat12.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat16.img bs=1M count=16
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 16 -i 0x12345678 build/test_mkfs/fat16.img
+    newfs_msdos -F 16 -I 0x12345678 build/test_mkfs/fat16.img
 else
     mkfs.fat -F 16 build/test_mkfs/fat16.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat32.img bs=1M count=100
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 32 -i 0x12345678 build/test_mkfs/fat32.img
+    newfs_msdos -F 32 -I 0x12345678 build/test_mkfs/fat32.img
 else
     mkfs.fat -F 32 build/test_mkfs/fat32.img -i 0x12345678
 fi
