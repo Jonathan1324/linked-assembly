4c46a01726b71ce2f3ca7373f15f37c7eedc5bc6
Jonathan1324 <mohrjonathan09@gmail.com>
2025-11-11 14:23:43 +0100

    .

--- test_lfs.sh
+++ test_lfs.sh
@@ -10,21 +10,27 @@ OS=$(uname)
 
 dd if=/dev/zero of=build/test_mkfs/fat12.img bs=1024 count=1440
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 12 -I 0x12345678 build/test_mkfs/fat12.img
+    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat12.img)
+    sudo newfs_msdos -F 12 "/dev/$MDDEV" -i 0x12345678
+    sudo mdconfig -d -u "$MDDEV"
 else
     mkfs.fat -F 12 build/test_mkfs/fat12.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat16.img bs=1M count=16
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 16 -I 0x12345678 build/test_mkfs/fat16.img
+    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat16.img)
+    sudo newfs_msdos -F 16 "/dev/$MDDEV" -i 0x12345678
+    sudo mdconfig -d -u "$MDDEV"
 else
     mkfs.fat -F 16 build/test_mkfs/fat16.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat32.img bs=1M count=100
 if [ "$OS" = "Darwin" ]; then
-    newfs_msdos -F 32 -I 0x12345678 build/test_mkfs/fat32.img
+    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat32.img)
+    sudo newfs_msdos -F 32 "/dev/$MDDEV" -i 0x12345678
+    sudo mdconfig -d -u "$MDDEV"
 else
     mkfs.fat -F 32 build/test_mkfs/fat32.img -i 0x12345678
 fi
