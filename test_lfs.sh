1dc61e91f318fcda294b2f19b4c1845df6a3d80d
Jonathan1324 <mohrjonathan09@gmail.com>
2025-11-11 14:25:29 +0100

    .

--- test_lfs.sh
+++ test_lfs.sh
@@ -10,27 +10,21 @@ OS=$(uname)
 
 dd if=/dev/zero of=build/test_mkfs/fat12.img bs=1024 count=1440
 if [ "$OS" = "Darwin" ]; then
-    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat12.img)
-    sudo newfs_msdos -F 12 "/dev/$MDDEV" -i 0x12345678
-    sudo mdconfig -d -u "$MDDEV"
+    echo "macOS not supported to create example images"
 else
     mkfs.fat -F 12 build/test_mkfs/fat12.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat16.img bs=1M count=16
 if [ "$OS" = "Darwin" ]; then
-    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat16.img)
-    sudo newfs_msdos -F 16 "/dev/$MDDEV" -i 0x12345678
-    sudo mdconfig -d -u "$MDDEV"
+    echo "macOS not supported to create example images"
 else
     mkfs.fat -F 16 build/test_mkfs/fat16.img -i 0x12345678
 fi
 
 dd if=/dev/zero of=build/test_mkfs/fat32.img bs=1M count=100
 if [ "$OS" = "Darwin" ]; then
-    MDDEV=$(sudo mdconfig -a -t vnode -f build/test_mkfs/fat32.img)
-    sudo newfs_msdos -F 32 "/dev/$MDDEV" -i 0x12345678
-    sudo mdconfig -d -u "$MDDEV"
+    echo "macOS not supported to create example images"
 else
     mkfs.fat -F 32 build/test_mkfs/fat32.img -i 0x12345678
 fi
